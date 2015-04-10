0402691b7f1113c9bf9082bb84125010257b6bb8
NingBell <xtzhongning@163.com>
2015-04-10 22:52:53 +0800

    changed the default command window size
    the exact size we can discuss tmr, i just add the code there, now its
    100*50

--- v0.3/V0.3/main.cpp
+++ v0.3/V0.3/main.cpp
@@ -8,6 +8,10 @@ using namespace std;
 const static string INVALID_COMMAND_MESSAGE ="Sorry, invalid command.\n\n";
 
 int main() {
+	system("mode 100,50");   //Set mode to ensure window does not exceed buffer size
+    SMALL_RECT WinRect = {0, 0, 100, 50};   //New dimensions for window in 8x12 pixel chars
+    SMALL_RECT* WinSize = &WinRect;
+    SetConsoleWindowInfo(GetStdHandle(STD_OUTPUT_HANDLE), true, WinSize); 
 	string message;
 	UI Minik;
 	cout << Minik.showWelcomeMessage();
